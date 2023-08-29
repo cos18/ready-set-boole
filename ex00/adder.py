fbf943858875990ceec5efefcc02984e0cb822c3
Sungwoo Park <codest99@gmail.com>
2023-08-29 17:18:07 +0900

    chore: implement ex00

--- ex00/adder.py
+++ ex00/adder.py
@@ -3,7 +3,28 @@ from beartype import beartype
 
 @beartype
 def adder(a: int, b: int) -> int:
-    pass
+    """
+    Recode addition method by bit operation.
+
+    Parameter of functions is limited by **32bit unsigned integer**
+    to secure O(1) time complexity.
+
+    Return value can be over 32bit unsigned integer value.
+
+    :param a: 32bit unsigned integer
+    :param b: 32bit unsigned integer
+    :return: addition result of params a and b
+    """
+    u32_max = 4294967295  # https://doc.rust-lang.org/std/primitive.u32.html
+    if a < 0 or b < 0 or a > u32_max or b > u32_max:
+        raise ValueError("parameter isn't 32bit unsigned integer range")
+    for _ in range(32):
+        carry = a & b
+        a ^= b
+        if not carry:
+            break
+        b = carry << 1
+    return a
 
 
 def main():
--- ex00/test_adder.py
+++ ex00/test_adder.py
@@ -5,24 +5,30 @@ from adder import adder
 
 
 class TestAdder:
+    u32_max = 4294967295
 
     def test_basic_addition(self):
         assert 10 + 20 == adder(10, 20)
+
+    def test_zero_addition(self):
+        for _ in range(100):
+            a = random.randint(0, self.u32_max)
+            assert a == adder(a, 0)
 
     def test_negative_exception(self):
         with pytest.raises(ValueError):
             adder(-1, -2)
 
-    def test_zero_exception(self):
+    def test_over_range_exception(self):
         with pytest.raises(ValueError):
-            adder(0, 1)
+            adder(0, self.u32_max + 1)
         with pytest.raises(ValueError):
-            adder(1, 0)
+            adder(self.u32_max + 1, 0)
         with pytest.raises(ValueError):
-            adder(0, 0)
+            adder(self.u32_max + 1, self.u32_max + 1)
 
     def test_random_addition(self):
         for _ in range(100):
-            a = random.randint(1, sys.maxsize)
-            b = random.randint(1, sys.maxsize)
+            a = random.randint(0, self.u32_max)
+            b = random.randint(0, self.u32_max)
             assert a + b == adder(a, b)
