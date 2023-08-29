16d25e92b74bd71b45b5313f2da8caaa4c4bd68a
Sungwoo Park <codest99@gmail.com>
2023-08-29 18:44:35 +0900

    chore: implement ex01

--- ex01/multiplier.py
+++ ex01/multiplier.py
@@ -1,9 +1,40 @@
 from beartype import beartype
+from ex00.adder import adder
 
 
 @beartype
 def multiplier(a: int, b: int) -> int:
-    pass
+    """
+    Recode multiplication method by bit operatior.
+
+    Parameter of functions is limited by **32bit unsigned integer**
+    to secure O(1) time complexity.
+
+    Return value can be over 32bit unsigned integer value.
+
+    :param a: 32bit unsigned integer
+    :param b: 32bit unsigned integer
+    :return: multiplication result of params a and b
+    """
+    u32_max = 4294967295
+    if a < 0 or b < 0 or a > u32_max or b > u32_max:
+        raise ValueError("parameter isn't 32bit unsigned integer range")
+
+    result = 0
+    mask = 0
+    for bits in range(32):
+        if b == 0:
+            break
+        if b & 1:
+            tmp = adder(result >> bits, a) << bits
+            remain = result & mask
+            result = tmp | remain
+        b = b >> 1
+        if mask:
+            mask = (mask << 1) | mask
+        else:
+            mask = 1
+    return result
 
 
 def main():
--- ex01/test_multiplier.py
+++ ex01/test_multiplier.py
@@ -1,28 +1,28 @@
 import random
-import sys
 import pytest
 from multiplier import multiplier
 
 
 class TestMultiplier:
+    u32_max = 4294967295
 
     def test_basic_multiplier(self):
-        assert 10 + 20 == multiplier(10, 20)
+        assert multiplier(10, 20) == 10 * 20
 
     def test_negative_exception(self):
         with pytest.raises(ValueError):
             multiplier(-1, -2)
 
-    def test_zero_exception(self):
+    def test_over_range_exception(self):
         with pytest.raises(ValueError):
-            multiplier(0, 1)
+            multiplier(0, self.u32_max + 1)
         with pytest.raises(ValueError):
-            multiplier(1, 0)
+            multiplier(self.u32_max + 1, 0)
         with pytest.raises(ValueError):
-            multiplier(0, 0)
+            multiplier(self.u32_max + 1, self.u32_max + 1)
 
     def test_random_multiplier(self):
         for _ in range(100):
-            a = random.randint(1, sys.maxsize)
-            b = random.randint(1, sys.maxsize)
-            assert a * b == multiplier(a, b)
+            a = random.randint(1, self.u32_max)
+            b = random.randint(1, self.u32_max)
+            assert multiplier(a, b) == a * b
