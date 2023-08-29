dddbcbec0a3360a36573e8eb5c0965a4b5014ce9
Sungwoo Park <codest99@gmail.com>
2023-08-29 18:25:22 +0900

    test: fix test code with clearity

--- ex00/test_adder.py
+++ ex00/test_adder.py
@@ -1,5 +1,4 @@
 import random
-import sys
 import pytest
 from adder import adder
 
@@ -8,12 +7,12 @@ class TestAdder:
     u32_max = 4294967295
 
     def test_basic_addition(self):
-        assert 10 + 20 == adder(10, 20)
+        assert adder(10, 20) == 10 + 20
 
     def test_zero_addition(self):
         for _ in range(100):
             a = random.randint(0, self.u32_max)
-            assert a == adder(a, 0)
+            assert adder(a, 0) == a
 
     def test_negative_exception(self):
         with pytest.raises(ValueError):
@@ -31,4 +30,4 @@ class TestAdder:
         for _ in range(100):
             a = random.randint(0, self.u32_max)
             b = random.randint(0, self.u32_max)
-            assert a + b == adder(a, b)
+            assert adder(a, b) == a + b
